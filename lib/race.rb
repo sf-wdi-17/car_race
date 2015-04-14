8e59d99c1529c69fb03f3cc25118480394129ced
Dennis Charles Hackethal <dennis@generalassemb.ly>
2015-04-14 12:31:38 -0700

    Implement Race

--- lib/race.rb
+++ lib/race.rb
@@ -1,3 +1,31 @@
 require_relative 'car'
 
-# Add Race class here
+class Race
+	attr_reader :cars
+
+	def initialize
+		@cars = []
+		go!
+	end
+
+	def winner
+		@cars.sort_by(&:speed).last
+	end
+
+	def loser
+		@cars.sort_by(&:speed).first
+	end
+
+private
+	def random_speed
+		(rand * 100).round
+	end
+
+	def go!
+		@cars << Car.new
+		@cars << Car.new
+
+		@cars.first.accelerate(random_speed)
+		@cars.last.accelerate(random_speed)
+	end
+end
